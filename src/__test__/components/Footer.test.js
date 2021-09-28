4ef0b3a6d7f6bfb41ecfbc33491bf0144ccf7b19
Ruben Rangel <rubenrang@gmail.com>
2021-09-28 10:54:40 -0500

    Testing Header Footer

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
   test('Render Footer component', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+
+  test('Render Title', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
